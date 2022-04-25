e41676861335a8c32bed5f3e1adf172416c90583
redouane <95357302+redelka00@users.noreply.github.com>
2022-04-25 06:01:53 +0000

    Create package_manager.sh

---
+++ Debian/package_manager.sh
@@ -0,0 +1,8 @@
+# This file include all the apps that are in the README
+# that can be installed via the default 'APT' package manager
+
+# *********** Browsers *********** #
+sudo apt install firefox
+
+# *********** Office *********** #
+sudo apt install libreoffice
