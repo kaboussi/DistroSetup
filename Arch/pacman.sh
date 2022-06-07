be36d30d08bf80604af3efa6eac5cd0928fe5d4a
redone <elkaboussi@pm.me>
2022-06-07 01:25:42 +0100

    add poetry

--- Arch/pacman.sh
+++ Arch/pacman.sh
@@ -26,3 +26,6 @@ sudo pacman -Syu sublime-text
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 echo 'export PATH="$HOME/.local/bin/:$PATH"' >> ~/.zshrc
 
+
+# Python Poetry
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
