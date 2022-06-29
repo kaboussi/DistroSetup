00152015d565d0f90a655bc9fc104c33c8edd5ad
Redone <elkaboussi@pm.me>
2022-06-29 20:46:30 +0100

    add miui bloatloader apps

---
+++ Miui/bloat.sh
@@ -0,0 +1,50 @@
+# Remove MIUI Bloatware apps 
+
+# Xiaomi
+adb shell pm uninstall -k --user 0 com.xiaomi.calendar
+adb shell pm uninstall -k --user 0 com.android.soundrecorder
+adb shell pm uninstall -k --user 0 com.android.providers.downloads.ui
+adb shell pm uninstall -k --user 0 com.micredit.in
+adb shell pm uninstall -k --user 0 com.mipay.wallet.in
+adb shell pm uninstall -k --user 0 com.xiaomi.scanner
+adb shell pm uninstall -k --user 0 com.mi.android.globalminusscreen
+
+
+# Miui
+adb shell pm uninstall -k --user 0 com.miui.calculator
+adb shell pm uninstall -k --user 0 com.miui.fm
+adb shell pm uninstall -k --user 0 com.miui.screenrecorder
+adb shell pm uninstall -k --user 0 com.miui.miservice
+adb shell pm uninstall -k --user 0 com.miui.notes
+adb shell pm uninstall -k --user 0 com.miui.compass
+
+
+# Google
+adb shell pm uninstall -k --user 0 com.google.android.youtube
+adb shell pm uninstall -k --user 0 com.google.android.gms.location.history
+adb shell pm uninstall -k --user 0 com.google.android.videos
+adb shell pm uninstall -k --user 0 com.google.android.music
+adb shell pm uninstall -k --user 0 com.google.android.apps.photos\t
+adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
+adb shell pm uninstall -k --user 0 com.google.ar.lens
+adb shell pm uninstall -k --user 0 com.google.android.apps.youtube
+adb shell pm uninstall -k --user 0 com.google.android.apps.docs
+adb shell pm uninstall -k --user 0 com.google.android.apps.messaging
+adb shell pm uninstall -k --user 0 com.android.chrome
+
+# Others
+adb shell pm uninstall -k --user 0 com.alibaba.aliexpresshd
+adb shell pm uninstall -k --user 0 com.ebay.mobile
+adb shell pm uninstall -k --user 0 com.netflix.partner.activation
+adb shell pm uninstall -k --user 0 com.facebook.appmanager
+adb shell pm uninstall --user 0 com.xiaomi.midrop
+adb shell pm uninstall -k --user 0 com.tencent.soter.soterserver
+adb shell pm uninstall -k --user 0 cn.wps.xiaomi.abroad.lite
+adb shell pm uninstall -k --user 0 com.miui.videoplayer
+adb shell pm uninstall -k --user 0 com.miui.player
+adb shell pm uninstall -k --user 0 com.mi.globalbrowser
+adb shell pm uninstall -k --user 0 com.miui.android.fashiongallery
+adb shell pm uninstall -k --user 0 com.miui.weather2\t
+adb shell pm uninstall -k --user 0 com.facebook.katana
+adb shell pm uninstall -k --user 0 com.facebook.services
+adb shell pm uninstall -k --user 0 com.facebook.system
